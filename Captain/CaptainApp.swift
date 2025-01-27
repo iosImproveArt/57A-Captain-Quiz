92a7dfaabd5f10163bb06014c838f483cd4a6023
iosImproveArt <impiosart@gmail.com>
2025-01-27 13:55:29 +0200

    init

---
+++ Captain/CaptainApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptainApp.swift
+//  Captain
+//
+//  Created by Improve on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CaptainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
